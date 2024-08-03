7a4ccf02545321539ea17d10b193ae3fd8e3da8a
yash <yshukla2775@gmail.com>
2024-08-03 14:38:37 +0530

    made some changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import HomeIcon from '@mui/icons-material/Home';
-import CodeIcon from '@mui/icons-material/Code';
 import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import LiveTvIcon from '@mui/icons-material/LiveTv';
@@ -16,11 +15,7 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  // { name: 'JS Mastery', icon: <CodeIcon />, },
-  // { name: 'Coding', icon: <CodeIcon />, },
-  // { name: 'ReactJS', icon: <CodeIcon />, },
-  // { name: 'NextJS', icon: <CodeIcon />, },
-  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Music', icon: <MusicNoteIcon /> ,},
   { name: 'Education', icon: <SchoolIcon />, },
   { name: 'Podcast', icon: <GraphicEqIcon />, },
   { name: 'Movie', icon: <OndemandVideoIcon />, },
@@ -34,9 +29,9 @@ export const categories = [
   { name: 'Crypto', icon: <DeveloperModeIcon />, },
 ];
 
-export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
-export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
-export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
-export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoThumbnailUrl = 'https://i3.ytimg.com/vi/U0GhFy9ZKjU/maxresdefault.jpg';
+export const demoChannelUrl = '/channel/UCUCzh0Do-xRlF0waqSvKN7A';
+export const demoVideoUrl = '/video/pcLRNHg5BYk';
+export const demoChannelTitle = 'Yash shukla';
+export const demoVideoTitle = 'Zindagi';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
